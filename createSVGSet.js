29e0a18afc2f7f079c441d78a727ebe39cc1ed81
hando <enthusapp@gmail.com>
2020-06-25 15:04:42 +0900

    fix two path icons

--- createSVGSet.js
+++ createSVGSet.js
@@ -25,22 +25,27 @@ const files = getAllFiles('material-design-icons');
 const filesKeys = Object.keys(files);
 const iconObject = {};
 let readIndex = 0;
+let failCount = 0;
+let sucessCount = 0;
 
 filesKeys.forEach(key => {
   fs.readFile(files[key], 'utf8', (err, data) => {
     const fdata = data.substring(data.indexOf('path') + 4, data.length - 9);
     const f2data = fdata.substring(fdata.indexOf(' d="') + 4);
 
-    if (f2data[0] !== 'M') {
-      console.log('tranfer fail');
+    if (f2data[0] !== 'M' || f2data.indexOf('path') > 0) {
+      failCount += 1;
+      console.log(`tranfer fail: ${failCount}`);
       console.log(data);
       console.log(`${key} === ${f2data}\r\n`);
     } else {
       iconObject[key] = f2data;
+      sucessCount += 1;
     }
 
     readIndex += 1;
     if (readIndex === filesKeys.length) {
+      console.log(`sucessCount: ${sucessCount}`);
       fs.writeFile(
         'test.js',
         `const icons = ${JSON.stringify(iconObject, null, 2)}`,
