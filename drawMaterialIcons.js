62f4a5be0d8918a9d37605ad9405533b73e3741f
enthusapp <enthusapp@gmail.com>
2021-12-07 17:33:58 +0900

    add opacity

--- drawMaterialIcons.js
+++ drawMaterialIcons.js
@@ -1,7 +1,12 @@
-module.exports = (context, { pathData, x, y, scale, color }) => {
+const addOpacity = (color, opacity = 1) => {
+  const ca = Array.isArray(color) ? color : hexToRgb(color);
+  return `RGB(${ca[0]}, ${ca[1]}, ${ca[2]}, ${opacity})`;
+};
+
+module.exports = (context, { pathData, x, y, scale, color }, opacity) => {
   context.save();
   context.translate(x, y);
-  context.fillStyle = color;
+  context.fillStyle = addOpacity(color, opacity);
   context.scale(scale, scale);
   context.fill(new Path2D(pathData));
   context.restore();
