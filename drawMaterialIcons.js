0965f0186adc8015f242cd40109dccb9b59b928c
enthusapp <enthusapp@gmail.com>
2021-12-07 17:36:24 +0900

    fix error

--- drawMaterialIcons.js
+++ drawMaterialIcons.js
@@ -1,3 +1,11 @@
+const hexToRgb = gr => {
+  const cs = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(gr);
+  const r = parseInt(cs[1], 16);
+  const g = parseInt(cs[2], 16);
+  const b = parseInt(cs[3], 16);
+  return [r, g, b];
+};
+
 const addOpacity = (color, opacity = 1) => {
   const ca = Array.isArray(color) ? color : hexToRgb(color);
   return `RGB(${ca[0]}, ${ca[1]}, ${ca[2]}, ${opacity})`;
